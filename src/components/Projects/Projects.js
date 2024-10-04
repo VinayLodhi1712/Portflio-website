c7ae81beb2d3387dd96e0f3400874cd88503a340
Vinay Anand Lodhi <vinayanandlodhi12@gmail.com>
2024-10-05 02:51:49 +0530

    projectsdone

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,8 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import spotify from "../../Assets/Projects/spotify.jpg";
 import netflix from "../../Assets/Projects/netflix.png";
+import nexdrive from "../../Assets/Projects/nexdrive.png";
+import dashboard from "../../Assets/Projects/dashboard.png";
 import iot from "../../Assets/Projects/iot.png";
 import Talkofcode from "../../Assets/Projects/talkofcode.png";
 import tempconverter from "../../Assets/Projects/tempconverter.png";
@@ -31,6 +33,7 @@ function Projects() {
               demoLink="https://talkofcode.vercel.app/"
             />
           </Col>
+          
 
           <Col md={4} className="project-card">
             <ProjectCard
@@ -39,10 +42,44 @@ function Projects() {
               title="Bookshop"
               description="This is a online book store e-commerce plateform which have basic functionalities of an ecommerce plateform made up using MERN Stack technology. It have basic CRUD operations for an author and users can buy books and add to cart books."
               ghLink="https://github.com/VinayLodhi1712/mern-book-store"
-             
+              demoLink="https://mern-book-store-qn8t.vercel.app/"
             />
           </Col>
 
+          <Col md={4} className="project-card">
+          <ProjectCard
+              imgPath={nexdrive}
+              isBlog={false}
+              title="Nexdrive"
+              description="What is NexDrive..? NexDrive is and Next-Gen Fleet Safety with Ai-powered safety technology. This is an IoT based product whose business model landing page I designed for the Smart India Hackathon'24."
+              ghLink="https://github.com/VinayLodhi1712/Nexdrive"
+              demoLink="https://nexdrive-774m.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={netflix}
+              isBlog={false}
+              title="Netflix Landing Page"
+              description="Netflix Landing Page Clone made with HTML and CSS. I have also made it responsive to work on various devices. This is the project where i started learning web dev from scratch. This is the landing page cone of famous movie streaming website Netflix"
+              ghLink="https://github.com/VinayLodhi1712/netflix-clone-landing-page"
+              demoLink="https://netflix-clone-landing-page-nine.vercel.app/"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={dashboard}
+              isBlog={false}
+              title="Dashboard Page"
+              description="Create a Dashboard page with Frontend Development to showcase my frontend development skills. It is a task of an online workshop of frontend development which i attended and done the task successfullly."
+              ghLink="https://github.com/VinayLodhi1712/dashboard"
+            />
+          </Col>
+
+          
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={iot}
@@ -63,6 +100,8 @@ function Projects() {
             />
           </Col>
 
+
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={tempconverter}
@@ -74,16 +113,7 @@ function Projects() {
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={netflix}
-              isBlog={false}
-              title="Netflix Landing Page"
-              description="Netflix Landing Page Clone made with HTML and CSS. I have also made it responsive to work on various devices. This is the project where i started learning web dev from scratch. This is the landing page cone of famous movie streaming website Netflix"
-              ghLink="https://github.com/VinayLodhi1712/netflix-clone-landing-page"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
+          
         </Row>
       </Container>
     </Container>
