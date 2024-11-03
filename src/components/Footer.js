81141b5fefe6342ef8b9a21db54464b154d269bf
Vinay Anand Lodhi <vinayanandlodhi12@gmail.com>
2024-11-03 11:30:52 +0530

    checking repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ import {
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaDiscord } from "react-icons/fa";
+
 function Footer() {
   let date = new Date();
   let year = date.getFullYear();
